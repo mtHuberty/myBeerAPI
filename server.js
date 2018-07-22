0f3da203c51f423ae45f648251de0963e0dbafd5
Matt Huberty <matthewhuberty77@gmail.com>
2018-07-22 17:55:13 -0500

    Added basic route and router to handle /api endpoint

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const beers = require('./app/routes/beers')
 
-// Allow key/value pairs to be more than strings/arrays
+// Allow key/value pairs to be more than strings/arrays, and tell our server to parse json
 app.use(express.urlencoded({ extended: true }))
-// Tells our server to parse requests as json
 app.use(express.json())
 
+// Middleware example
 app.use((req, res, next) => {
     console.log("Something's hitting me!")
     next()
@@ -14,8 +15,25 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log("What about me??")
+    next()
 })
 
+
+// Basic route
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to our beer API! Try hitting the /api endpoint' })
+})
+
+// Advanced routes
+const router = express.Router()
+
+app.use('/api', router)
+
+router.get('/', (req, res) => {
+    res.json({ message: 'Great! Now checkout the /api/beers endpoint. You can view and modify data there.'})
+})
+
+router.use('/beers', beers)
 
 
 const port = process.env.PORT || 9090
